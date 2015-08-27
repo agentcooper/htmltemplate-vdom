1a199872ef3b1568736f78fb6e1af005e1e7547b
Artem Tyurin <artem.tyurin@gmail.com>
2015-08-27 14:12:29 +0200

    Nested TMPL_LOOP test

--- test/test.js
+++ test/test.js
@@ -22,4 +22,21 @@ describe('htmltemplate-vdom', function() {
 
         assert.equal(vdom.children[0].children.length, 5);
     });
+
+    it('Nested TMPL_LOOP', function() {
+        var template = '<TMPL_LOOP items><span><TMPL_LOOP inner><TMPL_VAR title></TMPL_LOOP></span></TMPL_LOOP>';
+
+        var vdom = createVdom(template, {
+            items: [
+                { inner: [{ title: 'a1' }, { title: 'b1' }] },
+                { inner: [{ title: 'a2' }, { title: 'b2' }] },
+                { inner: [{ title: 'a3' }, { title: 'b3' }] }
+            ]
+        });
+
+        var outer = vdom.children[0].children;
+
+        assert.equal(outer.length, 3);
+        assert.equal(outer[0].children[0].children.length, 2);
+    });
 });
