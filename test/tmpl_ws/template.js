767d63d42882fd0e26885c2f7f1ac417dbd54772
Artem Tyurin <artem.tyurin@gmail.com>
2015-09-09 17:36:06 +0200

    TMPL_WS

---
+++ test/tmpl_ws/template.js
@@ -0,0 +1,64 @@
+function render(state, h) {
+    var lookupChain = [state];
+
+    function buildAttribute() {
+        return Array.prototype.slice.call(arguments).join('');
+    }
+
+    function tmpl_var(propertyName) {
+        return lookupValue(propertyName);
+    }
+
+    function tmpl_setvar(propertyName, value) {
+        lookupChain[lookupChain.length - 1][propertyName] = value;
+    }
+
+    function tmpl_call(name) {
+        var args = Array.prototype.slice.call(arguments, 1);
+
+        return lookupValue(name).apply(this, args);
+    }
+
+    function lookupValue(propertyName) {
+        var value = null;
+
+        for (var i = lookupChain.length - 1; i >= 0; i--) {
+            if (lookupChain[i][propertyName]) {
+                return lookupChain[i][propertyName];
+            }
+        }
+    }
+
+    function tmpl_loop(property, body, iterationVariableName) {
+        return lookupValue(property).map(function(item) {
+
+            if (iterationVariableName) {
+                var obj = {};
+                obj[iterationVariableName] = item;
+                lookupChain.push(obj);
+            } else {
+                lookupChain.push(item);
+            }
+
+            var iteration = body();
+
+            lookupChain.pop();
+
+            return iteration;
+        });
+    }
+
+    function perl_binary_expr(operator, left, right) {
+        if (operator === 'ne') {
+            return String(left) !== String(right);
+        }
+
+        if (operator === 'eq') {
+            return String(left) === String(right);
+        }
+
+        throw new Error(operator + ' is not implemented');
+    }
+
+return h('div', { 'className': buildAttribute('\n\n    item\n    ', '\n    item--active\n\n\n    ', '\n\n    item--green\n\n\n\n    ') }, ['\n    something\n']);
+}
